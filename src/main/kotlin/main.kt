9891bf8a01120a99ebd043208b9e32a10ce71c48
zhoujiaping <421211679@qq.com>
2021-01-26 20:23:44 +0800

    koans

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,4 +1,6 @@
 //https://play.kotlinlang.org/koans/Collections/Introduction/TestShop.kt
+//https://www.kotlincn.net/docs/reference/
+//
 fun main(){
     println("Hello world!")
     val name = readLine()
