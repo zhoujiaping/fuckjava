9205f892f31e06693f98ae2d2dcde0d3e8458c31
zhoujiaping <421211679@qq.com>
2021-02-05 20:46:43 +0800

    callback style code to sync style code

--- src/main/kotlin/coAction/co03-toSync.kt
+++ src/main/kotlin/coAction/co03-toSync.kt
@@ -4,13 +4,15 @@ import kotlinx.coroutines.async
 import kotlinx.coroutines.runBlocking
 import mylib.info
 import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
 import kotlin.coroutines.suspendCoroutine
 import kotlin.system.measureTimeMillis
+
 /**回调风格转同步风格*/
 fun main() {
     val time = measureTimeMillis {
-        runBlocking{
-            val defer = async{
+        runBlocking {
+            val defer = async {
                 mockCallback()
             }
             val res = defer.await()
@@ -19,26 +21,38 @@ fun main() {
     }
     println("cost:$time")
 }
-suspend fun mockCallback()=suspendCoroutine<String> {
-        Bus.info("...")
-        Bus.subscribe("hello"){
-            println("kitty")
-            it.resume("kitty")
+
+suspend fun mockCallback() = suspendCoroutine<String> {
+    Bus.info("...")
+    try {
+        Bus.subscribe("hello") {
+            try {
+                println("kitty")
+                it.resume("kitty")
+            } catch (e: Exception) {
+                it.resumeWithException(e)
+            }
         }
         Bus.publish("hello")
+    } catch (e: Exception) {
+        it.resumeWithException(e)
+    }
+
 }
+
 /**自定义事件总线，为了模拟回调
  * */
-class Bus{
-    companion object Bus{
-        private val handlers = mutableMapOf<String,MutableList<()->Unit>>()
-        fun subscribe(event:String, handler:()->Unit){
-            if(handlers[event]==null){
+class Bus {
+    companion object Bus {
+        private val handlers = mutableMapOf<String, MutableList<() -> Unit>>()
+        fun subscribe(event: String, handler: () -> Unit) {
+            if (handlers[event] == null) {
                 handlers[event] = mutableListOf()
             }
             handlers[event]!!.add(handler)
         }
-        fun publish(event:String){
+
+        fun publish(event: String) {
             handlers[event]!!.forEach {
                 it()
             }
